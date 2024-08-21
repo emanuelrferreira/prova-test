66f85c2f39f256a28b850a1dca81b8206d449af3
Emanuel Ferreira <162603680+emanuelrferreira@users.noreply.github.com>
2024-08-21 01:00:39 -0300

    javascript added

--- prova-ufv/script.js
+++ prova-ufv/script.js
@@ -1,1 +1,16 @@
-const paiDiv = doc
+// Seleciona todas as perguntas
+const allQuestions = document.querySelectorAll(".answertop");
+
+// Adiciona um evento de clique a cada pergunta
+allQuestions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+
+    // Alterna entre mostrar e esconder a resposta
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+});
